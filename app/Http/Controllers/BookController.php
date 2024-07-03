b69afce57daff25178cb9bdd0e3cf08d53ac075f
Rutger Bakker <rutger7bakker@gmail.com>
2024-07-03 16:03:37 +0200

    Started working on simple controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Books;
 
 class BookController extends Controller
 {
-    //
+    /**
+     * Get all books
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $books = Books::all();
+        return response()->json($books);
+    }
 }
