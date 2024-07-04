4220c12e5db6ac9b6045178b8e7043b416b88072
Rutger Bakker <rutger7bakker@gmail.com>
2024-07-04 16:13:41 +0200

    Make model and migration file

---
+++ app/Models/BlogPosts.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPosts extends Model
+{
+    use HasFactory;
+
+    protected $table = 'blog_posts';
+    protected $fillable = ['title', 'body', 'author'];
+}
