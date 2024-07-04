84dc93c8216d96b8adb49bf792a272e9a9a34e87
Rutger Bakker <rutger7bakker@gmail.com>
2024-07-04 10:27:08 +0200

    Added api

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/test', function () { return 'OK'; });
