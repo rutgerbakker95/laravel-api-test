a63b5d3dde88783c29abc0180c7b8f5c81a68c12
Rutger Bakker <rutger7bakker@gmail.com>
2024-07-05 15:05:41 +0200

    Filter on user_id and created_at date

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -8,11 +8,23 @@ use App\Models\BlogPost;
 class BlogPostController extends Controller
 {
     /**
-     * Display a listing of all blog posts.
+     * Display a listing of blog posts
      */
-    public function index()
+    public function index(Request $request)
     {
-        $blogPosts = BlogPost::with('user')->paginate(10);
+        $query = BlogPost::with('user');
+
+        if ($request->has('created_at')) {
+            $date = $request->input('created_at');
+            $query->whereDate('created_at', $date);
+        }
+
+        if ($request->has('user_id')) {
+            $userId = $request->input('user_id');
+            $query->where('user_id', $userId);
+        }
+
+        $blogPosts = $query->paginate(10);
 
         return response()->json($blogPosts);
     }
