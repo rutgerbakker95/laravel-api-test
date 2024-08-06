0664de4d10bb261dfc85fc697f88c5443ddde66f
Rutger Bakker <rutger7bakker@gmail.com>
2024-08-06 10:35:39 +0200

    Implemented vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+          template: {
+            transformAssetUrls: {
+              base: null,
+              includeAbsolute: false,
+            },
+        },
+      }),
     ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+      }
+    }
 });
